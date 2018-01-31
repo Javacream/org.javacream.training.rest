5e0fe7a95a7407277ce8d5eae984639ff7ec7c9d
Rainer <training@rainer-sawitzki.de>
2018-01-31 13:44:37 +0100

    fix bug, add @RequestBody

--- org.javacream.training.rest.people.server/src/main/java/org/javacream/training/application/PeopleRestController.java
+++ org.javacream.training.rest.people.server/src/main/java/org/javacream/training/application/PeopleRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,12 +30,12 @@ public class PeopleRestController{
 	}
 
 	@RequestMapping(method=RequestMethod.POST, path="/people", consumes="application/json")
-	public void savePerson(Person p) {
+	public void savePerson(@RequestBody Person p) {
 		repo.save(p);
 	}
 
 	@RequestMapping(method=RequestMethod.PUT, path="/people", consumes="application/json")
-	public void updatePerson(Person p) {
+	public void updatePerson(@RequestBody Person p) {
 		repo.save(p);
 	}
 
