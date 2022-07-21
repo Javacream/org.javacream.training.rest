ef8144c7ee373837695dcc6774b05046e5337611
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-21 12:39:24 +0200

    add @CrossOrigin

--- org.javacream.training.rest.books.server/src/main/java/org/javacream/store/web/StoreWebService.java
+++ org.javacream.training.rest.books.server/src/main/java/org/javacream/store/web/StoreWebService.java
@@ -3,6 +3,7 @@ package org.javacream.store.web;
 import org.javacream.store.api.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api")
+@CrossOrigin
 public class StoreWebService {
 
 	@Autowired
